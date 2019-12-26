8f881512b0da7fa7a6cb80b2e08993aef59f9725
eddieruiz <eddi@overseasinternational.com>
2019-12-26 15:03:46 -0500

    starting of hotelInfo

--- Server/client/src/components/HotelList.js
+++ Server/client/src/components/HotelList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Accordion } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import HotelInfo from './HotelInfo';
 
 export default class HotelList extends Component {
   render() {
@@ -11,9 +12,13 @@ export default class HotelList extends Component {
           <h1>HOTEL LIST COMPONENT</h1>
           <Accordion defaultActiveKey="0">
             <Card>
+
               <Card.Header>
+            
+              <HotelInfo />
+              <h2>Marriott Grande Vista</h2>
                 <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                  Click me!
+                  Rooms/Availability
                 </Accordion.Toggle>
               </Card.Header>
               <Accordion.Collapse eventKey="0">
@@ -23,10 +28,30 @@ export default class HotelList extends Component {
             <Card>
               <Card.Header>
                 <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                  Click me!
+                  Rooms/Availability
                 </Accordion.Toggle>
               </Card.Header>
               <Accordion.Collapse eventKey="1">
+                <Card.Body>Hello! I'm the body</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="2">
+                  Rooms/Availability
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="2">
+                <Card.Body>Hello! I'm the body</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="3">
+                Rooms/Availability
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="3">
                 <Card.Body>Hello! I'm another body</Card.Body>
               </Accordion.Collapse>
             </Card>
