f4dab73ba0e9d46d967d340ed4ff6d0743c36328
eddieruiz <eddi@overseasinternational.com>
2019-12-25 23:55:20 -0500

    hotel list structure added to react front end

--- Server/client/src/App.js
+++ Server/client/src/App.js
@@ -2,23 +2,32 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import HotelList from "./components/HotelList";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="jumbotron">
+        <h3>This test was made with these technologies</h3>
+        <div className="row">
+          <div className="col-md-3">
+            <p>Insert React picture</p>
+          </div>
+          <div className="col-md-3">
+            <p>Insert Mongo Db picture</p>
+          </div>
+          <div className="col-md-3">
+            <p>Insert Express picture</p>
+          </div>
+          <div className="col-md-3">
+            <p>Insert Node picture</p>
+          </div>
+        </div>
+      </div>
+      <div className="hotelList">
+        <HotelList />
+      </div>
     </div>
   );
 }
--- Server/client/src/components/HotelList.js
+++ Server/client/src/components/HotelList.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class HotelList extends Component {
   render() {
     return (
       <div>
-        
+        <div className="container">
+          <h1>HOTEL LIST COMPONENT</h1>
+        </div>
       </div>
-    )
+    );
   }
 }
