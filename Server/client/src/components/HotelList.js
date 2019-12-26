6d892cfd30bbb2dfa29410596b1d0b4b30d74bef
eddieruiz <eddi@overseasinternational.com>
2019-12-26 00:22:36 -0500

    added hotel list to be styled

--- Server/client/src/components/HotelList.js
+++ Server/client/src/components/HotelList.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { Accordion } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 export default class HotelList extends Component {
   render() {
@@ -6,6 +9,28 @@ export default class HotelList extends Component {
       <div>
         <div className="container">
           <h1>HOTEL LIST COMPONENT</h1>
+          <Accordion defaultActiveKey="0">
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                  Click me!
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>Hello! I'm the body</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                  Click me!
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>Hello! I'm another body</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
         </div>
       </div>
     );
