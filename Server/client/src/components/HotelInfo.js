6bf053911cb55b70490df0143ec8040fea781c31
eddieruiz <eddi@overseasinternational.com>
2019-12-29 23:16:08 -0500

    added a few styles

--- Server/client/src/components/HotelInfo.js
+++ Server/client/src/components/HotelInfo.js
@@ -69,21 +69,22 @@ export default class HotelInfo extends Component {
                   {this.props.hotels.hotelRooms[0].availability}
                 </div>
                 <div className="col-md-3">
-                  {this.props.hotels.hotelRooms[0].price}
+                {this.props.hotels.hotelRooms[0].details}
+              
                 </div>
                 <div className="col-md-3">
-                  {this.props.hotels.hotelRooms[0].details}
+                {this.props.hotels.hotelRooms[0].price}
                 </div>
               </div>
-              <br></br>
               <br></br>
               <div className="row">
                 <div className="col-md-4">
                   {this.props.hotels.hotelRooms[1].roomName}
                 </div>
-                <div className="col-md-2">Availability</div>
-                <div className="col-md-3">Price</div>
-                <div className="col-md-3">Details</div>
+                <div className="col-md-2">{this.props.hotels.hotelRooms[1].availability}</div>
+                <div className="col-md-3">{this.props.hotels.hotelRooms[1].details}</div>
+                <div className="col-md-3">{this.props.hotels.hotelRooms[1].price}</div>
+          
               </div>
             </Card.Body>
           </Accordion.Collapse>
